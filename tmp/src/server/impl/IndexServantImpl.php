6f65efa1cf0aa3a3921ec7f513d1114a9784ceab
李俊东 <lijundong@51zhaoyou.com>
2018-12-23 10:21:42 +0800

    23
    12

--- tmp/src/server/impl/IndexServantImpl.php
+++ tmp/src/server/impl/IndexServantImpl.php
@@ -2,6 +2,7 @@
 namespace src\Impl;
 use src\common\Common;
 use src\servant\${doTarsServerName}\${doTarsServantName}\${doTarsObjName}\${doTarsServantImplName};
+${doTarsUseCode}
 
 class IndexServantImpl implements ${doTarsServantImplName}
 {
